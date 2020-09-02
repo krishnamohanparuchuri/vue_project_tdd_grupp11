9ddff99e84e4efbc4aec140819afa817a3551c68
Razieh <razie.rouzbahani@gmail.com>
2020-09-02 11:23:53 +0200

    modify aboutpage homepage razieh

--- src/views/aboutpage.vue
+++ src/views/aboutpage.vue
@@ -1,20 +1,55 @@
 <template>
   <div class="container">
-      <h1>about page</h1>
-      <div>
-        hej
+    <NavigationPage />
+    <ShoppingBag />
+      <div class="wrapper">
+        <img class="" src="" alt="logo">
+        <h1 class="title">{{ title }}</h1>
+        <p class="description">{{ description }}</p>
       </div>
   </div>
 </template>
 
 <script>
+
+import NavigationPage from '@/components/NavigationPage.vue';
+import ShoppingBag from '@/components/ShoppingBag.vue';
+
 export default {
-    name: "aboutpage"
+    name: 'aboutpage',
+    components: {
+  NavigationPage, 
+  ShoppingBag
+},
+    data() {
+      return {
+        title: 'About us',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+      };
+     
+    }
 }
 </script>
 
-<style>
+<style scoped>
+.container {
+  background: #50a280;
+  width: auto;
+}
+.wrapper {
+  padding: 20px 32px;
+}
 h1{
-  color: black
+  color: white;
+  font-family: 'PT Serif', serif;
+  font-weight: 600;
+}
+.description {
+  font-weight: bold;
+  margin: 20px 0;
+}
+#text2 {
+  font-size: 1em;
+  margin-bottom: 20px;
 }
 </style>
--- src/views/homepage.vue
+++ src/views/homepage.vue
@@ -1,11 +1,19 @@
 <template>
   <div class="container">
+    <NavigationPage />
+    <ShoppingBag />
       <h1>homepage</h1>
   </div>
 </template>
 
 <script>
+import NavigationPage from '@/components/NavigationPage.vue';
+import ShoppingBag from '@/components/ShoppingBag.vue';
+ 
 export default {
+  components: {
+    NavigationPage, ShoppingBag
+  }
 
 }
 </script>
