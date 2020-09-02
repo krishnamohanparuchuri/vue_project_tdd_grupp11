5a1117db29ca673ae1dc409f3e25d8cdd4bb3811
krishnamohanparuchuri <55274056+krishnamohanparuchuri@users.noreply.github.com>
2020-09-02 13:49:27 +0200

    newfiles for homepage and productspage

--- src/views/homepage.vue
+++ src/views/homepage.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="container">
-    <NavigationPage />
-    <ShoppingBag />
+    <div class="container-header">
+      <NavigationPage />
+      <ShoppingBag />
+    </div>
     <h1>homepage</h1>
   </div>
 </template>
@@ -19,4 +21,16 @@ export default {
 };
 </script>
 
-<style></style>
+<style scoped>
+.container {
+  background: #50a280;
+}
+
+.container-header {
+  display: flex;
+  /* background: linear-gradient(to bottom, #00ff95, #00ce78);
+
+   */
+  background: #50a280;
+}
+</style>
--- src/views/productspage.vue
+++ src/views/productspage.vue
@@ -1,18 +1,20 @@
 <template>
-  <div>
+  <div class="container">
     <NavigationPage />
     <ShoppingBag />
     <h1>Products</h1>
-    <ul>
-      <li v-for="product in products" :key="product.id">
-        <h1>{{ product.title }}</h1>
-        <h4>{{ product.color }}</h4>
-        <h3>{{ product.price }}</h3>
-        <button>+</button>
-        <p>Quantity</p>
-        <button>-</button>
-      </li>
-    </ul>
+    <div class="product-div">
+      <ul>
+        <li v-for="product in products" :key="product.id">
+          <h1>{{ product.title }}</h1>
+          <h4>{{ product.color }}</h4>
+          <h3>{{ product.price }}</h3>
+          <button>+</button>
+          <p>Quantity</p>
+          <button>-</button>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -42,4 +44,26 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+.container {
+  background: #50a280;
+}
+
+.product-div {
+  display: flex;
+  flex-wrap: wrap;
+  background: #50a280;
+}
+
+ul {
+  width: 80%;
+}
+li {
+  background: whitesmoke;
+  box-shadow: 5px 5px 6px;
+  margin: 10px;
+  border-radius: 7.25px;
+
+  /* margin: 0 auto; */
+}
+</style>
